8d54a6ad7e12ca3e55d99c77f243a11c07744fae
Santiago <santiifonzalida@gmail.com>
2024-05-20 10:30:08 -0300

    config main module, Joi validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { environments } from './environments';
+import config from './config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
 import { MessagesService } from './messages/services/messages.service';
 
+
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
+    }), 
+    MessagesModule
+  ],
   controllers: [AppController],
   providers: [AppService, MessagesService],
 })
