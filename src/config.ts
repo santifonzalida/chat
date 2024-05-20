4f270ae43fc2379258325bbfd23ed2d934f7fd83
Santiago <santiifonzalida@gmail.com>
2024-05-20 09:51:25 -0300

    mongo config env

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+import { registerAs } from "@nestjs/config";
+
+export default registerAs('config', () => {
+    return {
+        mongo: {
+            dbName: process.env.MONGO_DB,
+            user: process.env.MONGO_INITDB_ROOT_USERNAME,
+            password: process.env.MONGO_INITDB_ROOT_PASSWORD,
+            host: process.env.MONGO_HOST,
+            connection: process.env.MONGO_CONNECTION,
+        }
+    }
+})
